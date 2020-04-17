80872ef555b426112bbbd1756b16d10280f7d803
shaoxi <shaoxi277214@foxmail.com>
2020-04-17 11:33:41 +0800

    二叉树序列化和解析

--- src/solution/Solution8.java
+++ src/solution/Solution8.java
@@ -1,12 +1,69 @@
 package solution;
 
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * create at 20200413
  */
 public class Solution8 {
+    //序列化二叉树
+    //LinkedList类实现了Queue接口
+    private int indexx=-1;
+
+    String Serialize(TreeNode root) {
+        if (root==null){
+            return "#";
+        }
+        //这里可以改成‘！’
+        return root.val+","+Serialize(root.left) +","+ Serialize(root.right);
+    }
+    TreeNode Deserialize(String str) {
+        String[] s=str.split(",");
+        indexx++;
+        int len = s.length;
+        if(indexx >=len){
+            return null;
+        }
+        TreeNode t=null;
+        // s 中的元素还是字符串
+        if(!s[indexx].equals("#")){
+            //t=TreeNode(Integer.parseInt(s[indexx]));
+            t= new TreeNode(Integer.parseInt(s[indexx]));
+            t.left=Deserialize(str);
+            t.right=Deserialize(str);
+
+
+        }
+        return t;
+
+
+    }
+    private  int index=0;
+    TreeNode KthNode(TreeNode pRoot, int k)
+    {//中序遍历
+        if (pRoot!=null){
+            TreeNode node = KthNode(pRoot.left,k);
+            if(node!=null){
+                return node;
+            }
+            index++;
+
+            if(index==k){
+                return pRoot;
+            }
+            node = KthNode(pRoot.right,k);
+            if(node!=null){
+                return node;
+            }
+        }
+        return null;
+
+    }
+
+
     private PriorityQueue<Integer> minHeap =new PriorityQueue<>();
     private PriorityQueue<Integer> maxHeap =new PriorityQueue<>(15,new Comparator<Integer>() {
         @Override
--- src/solution/SolutionTest.java
+++ src/solution/SolutionTest.java
@@ -154,6 +154,32 @@ class SolutionTest {
        // System.out.printf(s.GetMedian());
        assert s.GetMedian()==2.5;
     }
+    @Test
+    void testse(){
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode5 = new TreeNode(5);
+        TreeNode treeNode6 = new TreeNode(6);
+
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode3;
+
+        treeNode2.left = treeNode4;
+        treeNode3.left = treeNode5;
+        treeNode3.right = treeNode6;
+
+        Solution8 serializeTree = new Solution8();
+
+        String str = serializeTree.Serialize(treeNode1);
+
+        TreeNode treeNode = serializeTree.Deserialize(str);
+
+
+
+
+    }
 
 
 }
