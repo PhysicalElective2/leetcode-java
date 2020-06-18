c5d2d2ca2f9042f542fdf5de2fe1fbc80496e371
shaoxi <shaoxi277214@foxmail.com>
2020-06-18 19:45:01 +0800

    最长回文子串，将以前python 写法转为java

--- src/company/TencentSolution.java
+++ src/company/TencentSolution.java
@@ -1,9 +1,39 @@
 package company;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TencentSolution {
+    public String spread(String s,int start,int end){
+        //从中心向两边扩展
+        StringBuilder res=new StringBuilder();
+        while(start>=0&&end<s.length()&&s.charAt(start)==s.charAt(end)){
+            start--;
+            end++;
+        }
+        for(int i=start+1;i<end;i++){
+            res.append(s.charAt(i));
+
+        }
+        return res.toString();
+
+    }
+    public String longestPalindrome(String s) {
+        //最长回文子串
+        String res="";
+        for(int i=0;i<s.length();i++){
+            String res1=spread(s,i,i);
+            String res2=spread(s,i,i+1);
+            res=res.length()>res1.length()?res:res1;
+            res=res.length()>res2.length()?res:res2;
+        }
+        return res;
+
+
+    }
+
     public int[] twoSum(int[] nums, int target) {
 //        List<Integer> res=new ArrayList<Integer>();
         int[] res={-1,-1};
@@ -29,6 +59,7 @@ public class TencentSolution {
         for (int i = 0; i <res.length; i++) {
             System.out.println(res[i]);
         }
+        System.out.println(t.longestPalindrome("cdbbdccc"));
     }
 
 }
