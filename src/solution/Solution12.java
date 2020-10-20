52a1cbe59ba3e684f5f44df87605bbac3f684b1c
shaoxi <shaoxi277214@foxmail.com>
2020-10-20 19:27:47 +0800

    always have little wrong

--- src/solution/Solution12.java
+++ src/solution/Solution12.java
@@ -1,25 +1,57 @@
 package solution;
 
+import java.util.ArrayList;
+
 public class Solution12 {
+    public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
+        //
+        ArrayList<ListNode> array = new ArrayList<ListNode>();
+        ListNode anotherHead = head;
+
+        while (anotherHead != null) {
+            array.add(anotherHead);
+            anotherHead = anotherHead.next;
+        }
+
+        ListNode p = head;
+        p.next = array.get(array.size() - 1);
+        p = p.next;
+        for (int i = 1; i < array.size() / 2; i++) {
+            p.next = array.get(i);
+            p = p.next;
+            p.next = array.get(array.size() - i - 1);
+            p = p.next;
+        }
+        if (array.size() % 2 == 1) {
+            p.next = array.get(array.size() / 2);
+            p = p.next;
+        }
+        p.next = null;
+
+    }
+
     public boolean backspaceCompare(String S, String T) {
-        StringBuilder Sa=new StringBuilder();
-        StringBuilder Sb=new StringBuilder();
+        StringBuilder Sa = new StringBuilder();
+        StringBuilder Sb = new StringBuilder();
 //        StringBuffer Sb=new StringBuffer();
-        for(int i=0;i<S.length();i++){
-            if(S.charAt(i)=='#'){
-                if(Sa.length()>0){
-                    Sa.deleteCharAt(Sa.length()-1);
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '#') {
+                if (Sa.length() > 0) {
+                    Sa.deleteCharAt(Sa.length() - 1);
                 }
-            }else{
+            } else {
                 Sa.append(S.charAt(i));
             }
         }
-        for(int i=0;i<T.length();i++){
-            if(T.charAt(i)=='#'){
-                if(Sb.length()>0){
-                    Sb.deleteCharAt(Sb.length()-1);
+        for (int i = 0; i < T.length(); i++) {
+            if (T.charAt(i) == '#') {
+                if (Sb.length() > 0) {
+                    Sb.deleteCharAt(Sb.length() - 1);
                 }
-            }else{
+            } else {
                 Sb.append(T.charAt(i));
             }
         }
