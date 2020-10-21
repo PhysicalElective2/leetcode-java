3c6b09d6666ae1ae61a4b95c55535c879e74f012
shaoxi <shaoxi277214@foxmail.com>
2020-10-21 20:38:52 +0800

    always have little wrong

--- src/solution/Solution12.java
+++ src/solution/Solution12.java
@@ -3,6 +3,23 @@ package solution;
 import java.util.ArrayList;
 
 public class Solution12 {
+    public boolean isLongPressedName(String name, String typed) {
+        int i=0;
+        int j=0;
+        while (j<typed.length()){
+            if(i<name.length()&&name.charAt(i)==typed.charAt(j)){
+                i++;
+                j++;
+            }else if(j>0&&typed.charAt(j-1)==typed.charAt(j)){
+                j++;
+            }else {
+               return  false;
+
+            }
+        }
+
+        return i==name.length();
+    }
     public void reorderList(ListNode head) {
         if (head == null) {
             return;
