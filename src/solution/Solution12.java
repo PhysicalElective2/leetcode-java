22767355940d372c6f5db458d3c065413ad8006c
shaoxi <shaoxi277214@foxmail.com>
2020-10-30 18:59:44 +0800

    easy

--- src/solution/Solution12.java
+++ src/solution/Solution12.java
@@ -4,6 +4,32 @@ import java.lang.reflect.Field;
 import java.util.*;
 
 public class Solution12 {
+    public int islandPerimeter(int[][] grid) {
+        //
+        int res=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+                if(grid[i][j]==1){
+                    res+=num(grid,i,j);
+                }
+
+            }
+        }
+        return res;
+
+
+    }
+    public int num(int[][] grid,int i,int j){
+        //find its edge of a stone
+        int res=0;
+        if(i-1<0||grid[i-1][j]==0)res++;
+        if(i+1>=grid.length||grid[i+1][j]==0)res++;
+        if(j-1<0||grid[i][j-1]==0)res++;
+        if(j+1>=grid[i].length||grid[i][j+1]==0)res++;
+
+        return res;
+
+    }
     public boolean uniqueOccurrences(int[] arr) {
         //map is key and value
         //set is unique
