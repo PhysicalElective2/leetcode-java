7532b111749a164ca11995f00ad3fb57cd32fbef
shaoxi <shaoxi277214@foxmail.com>
2020-11-10 19:30:40 +0800

    next permutation is pretty hard

--- src/solution/Solution13.java
+++ src/solution/Solution13.java
@@ -22,6 +22,41 @@ public class Solution13 {
         System.out.println("当前时间："+simpleDateFormattime.format(simpdate));
 
     }
+    public void nextPermutation(int[] nums) {
+        int i=nums.length-2;
+        while (i>=0&&nums[i]>=nums[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j=nums.length-1;
+            while (j>=0&&nums[j]<=nums[i]){
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1);
+
+
+    }
+
+    private void reverse(int[] nums, int i) {
+        int left =i;
+        int right =nums.length-1;
+        while (left<right){
+            swap(nums,left,right);
+            left++;
+            right--;
+
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        
+    }
+
     public int[][] kClosest(int[][] points, int K) {
         int[][] res=new int[K][2];
         int[] lens =new int[points.length];
