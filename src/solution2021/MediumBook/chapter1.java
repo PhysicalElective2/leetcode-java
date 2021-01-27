7abd2fb461241a8f5f57ccd2a37c7bdd81349d33
shaoxi <shaoxi277214@foxmail.com>
2021-01-27 21:33:37 +0800

    continue pursue

--- src/solution2021/MediumBook/chapter1.java
+++ src/solution2021/MediumBook/chapter1.java
@@ -1,7 +1,152 @@
 package solution2021.MediumBook;
 
+import java.util.*;
+
 public class chapter1 {
+    public static void main(String[] args) {
+        chapter1 c=new chapter1();
+//        System.out.println(c.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(c.longestPalindrome("babad"));
+        Integer i=0;
+        Boolean j=false;
+        int a;
+        System.out.println(a=10);
+    }
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+
+        int[] dp =new int[n];
+        Arrays.fill(dp,1);
+        int res=dp[0];
+        for(int i=1;i<n;i++){
+
+
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+
+            }
+            res=Math.max(res,dp[i]);
+            if(res>=3) return true;
+        }
+        return false;
+
+
+
+    }
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+
+        int[] dp =new int[n];
+        Arrays.fill(dp,1);
+        int res=dp[0];
+        for(int i=1;i<n;i++){
+
+
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+
+            }
+            res=Math.max(res,dp[i]);
+        }
+        return res;
+    }
+    public String longestPalindrome(String s) {
+        int n =s.length();
+        String res="";
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=1;j++){
+                int u=i;
+                int v=i+j;
+                while (u>=0&&v<n&&s.charAt(u)==s.charAt(v)){
+                    u--;
+                    v++;
+                }
+                if(v-u-1>res.length()){
+                    res=s.substring(u+1,v);
+                }
+
+            }
+        }
+        return res;
+
+    }
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int end = 0;
+        int maxL=1;
+        int n=s.length();
+        if(n==0) return 0;
+        for(int i=0;i<n;i++){
+            if(i!=0){
+                set.remove(s.charAt(i-1));
+            }
+            while (end<n&&!set.contains(s.charAt(end))){
+                set.add(s.charAt(end));
+                end++;
+
+            }
+            maxL=Math.max(maxL,set.size());
+        }
+
+        return maxL;
+
+
+
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map =new HashMap<>();
+        for(String s:strs){
+
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key =new String(chars);
+
+
+
+            List<String> list=map.getOrDefault(key,new ArrayList<String>());
+            list.add(s);
+            map.put(key,list);
+        }
+        //creat list by map.values
+        return new ArrayList<>(map.values());
+
+    }
+
     public void setZeroes(int[][] matrix) {
+        int line1Flag = 1;
+        int m = matrix.length;
+        if (m == 0) return;
+        int n = matrix[0].length;
+        if (n == 0) return;
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                line1Flag = 0;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = m-1; i >=1; i--) {
+            for (int j = n-1; j >=0; j--) {
+                if (matrix[i][0] == 0||matrix[0][j]==0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = n-1; j >=0; j--) {
+            if (line1Flag == 0) {
+                matrix[0][j]=0;
+            }
+        }
 
 
     }
--- src/solution2021/S2.java
+++ src/solution2021/S2.java
@@ -12,12 +12,146 @@ import java.util.*;
 
 public class S2 {
     public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
-        Stack s=new Stack();
+        Stack s = new Stack();
 //        s.add();
         return new int[0];
 
 
     }
+// wrong, because have negative number
+    public int subarraySum2(int[] nums, int k) {
+        Map<Integer,Integer> map =new HashMap<>();
+        map.put(0,1);
+        int res=0;
+        int pre =0;
+        for(int i=0;i<nums.length;i++){
+            pre+=nums[i];
+            if(map.containsKey(pre-k)){
+                res+=map.get(pre-k);
+            }
+            map.put(pre,map.getOrDefault(pre,0)+1);
+
+        }
+        return res;
+
+
+    }
+    public int subarraySum(int[] nums, int k) {
+        int res = 0;
+        int n = nums.length;
+        if (n == 0) return res;
+        int tempSum = 0;
+        int startI = 0;
+        tempSum += nums[startI];
+        if(tempSum==k) res++;
+        for (int i = 1; i < nums.length; i++) {
+            // i as end
+            tempSum+=nums[i];
+            if (tempSum < k) {
+                continue;
+            }
+
+            while (tempSum>k){
+                tempSum -= nums[startI];
+                startI++;
+
+            }
+            if (tempSum == k) {
+                res++;
+                tempSum -= nums[startI];
+                startI++;
+                continue;
+
+            }
+
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        S2 s = new S2();
+        System.out.println(s.largestRectangleArea(new int[]{2, 1, 5, 6, 2, 3}));
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int res = 0;
+        int n = heights.length;
+        for (int i = 0; i < n; i++) {
+            int len = heights[i];
+
+            for (int j = i; j >= 0; j--) {
+
+                len = Math.min(len, heights[j]);
+                res = Math.max(res, len * (i - j + 1));
+
+            }
+
+        }
+        return res;
+
+    }
+
+    public int largestRectangleArea2(int[] heights) {
+        int res = 0;
+        int n = heights.length;
+        if (n == 0) return 0;
+
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+            int left = i;
+            while (left > 0 && heights[left - 1] >= height) left--;
+            int right = i;
+            while (right < n - 1 && heights[right + 1] >= height) right++;
+            res = Math.max(res, height * (right - left + 1));
+
+
+        }
+        return res;
+
+    }
+
+    public int largestRectangleArea3(int[] heights) {
+        int res = 0;
+        int n = heights.length;
+        if (n == 0) return 0;
+        int[] temp = new int[n + 2];
+        System.arraycopy(heights, 0, temp, 1, n);
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < temp.length; i++) {
+            while (!stack.isEmpty() && temp[i] < temp[stack.peek()]) {
+                int h = temp[stack.pop()];
+                res = Math.max(res, (i - stack.peek() - 1) * h);
+            }
+            stack.push(i);
+
+
+        }
+        return res;
+
+    }
+
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] num = new int[100];
+        int res = 0;
+        for (int[] i : dominoes) {
+            int val = i[0] > i[1] ? i[0] * 10 + i[1] : i[1] * 10 + i[0];
+            res += num[val];
+            num[val]++;
+        }
+        return res;
+
+
+    }
+
+
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[nums.length - 1], nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3]);
+
+
+    }
+
     public int minCostConnectPoints2(int[][] points) {
         int[][] graph = new int[points.length][points.length];
         int[] weights = new int[points.length];
@@ -32,7 +166,7 @@ public class S2 {
         for (int i = 1; i < points.length; i++) {
             int k = 0, min = Integer.MAX_VALUE;
             for (int j = 0; j < points.length; j++) {
-                if(weights[j] != 0 && weights[j] < min){
+                if (weights[j] != 0 && weights[j] < min) {
                     min = weights[j];
                     k = j;
                 }
@@ -40,44 +174,44 @@ public class S2 {
             result += weights[k];
             weights[k] = 0;
             for (int j = 0; j < points.length; j++) {
-                if(weights[j] != 0 && graph[k][j] < weights[j]){
+                if (weights[j] != 0 && graph[k][j] < weights[j]) {
                     weights[j] = graph[k][j];
                 }
             }
         }
         return result;
     }
-    public int minCostConnectPoints(int[][] points) {
-        int n= points.length;
 
-        Dunion union =new Dunion(n);
-        List<Edge> edges =new ArrayList<>();
-        for (int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                edges.add(new Edge(dist(points,i,j),i,j));
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+
+        Dunion union = new Dunion(n);
+        List<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                edges.add(new Edge(dist(points, i, j), i, j));
             }
         }
         Collections.sort(edges, new Comparator<Edge>() {
             @Override
             public int compare(Edge o1, Edge o2) {
-                return o1.len-o2.len;
+                return o1.len - o2.len;
             }
         });
-        int res=0;
-        int num=0;
-        for(Edge edge:edges){
-            int len= edge.len;
-            int x= edge.x;
-            int y= edge.y;
-            if(union.unionSet(x,y)){
-                res+=len;
+        int res = 0;
+        int num = 0;
+        for (Edge edge : edges) {
+            int len = edge.len;
+            int x = edge.x;
+            int y = edge.y;
+            if (union.unionSet(x, y)) {
+                res += len;
                 num++;
-                if(num==n){
+                if (num == n) {
                     break;
                 }
             }
         }
-
 
 
         return res;
@@ -90,7 +224,7 @@ public class S2 {
 
     }
 
-    class Edge{
+    class Edge {
         int len, x, y;
 
         public Edge(int len, int x, int y) {
@@ -100,24 +234,27 @@ public class S2 {
         }
 
     }
-    class Dunion{
+
+    class Dunion {
         int[] f;
         int[] rank;
         int n;
 
 
         public Dunion(int n) {
-            this.n=n;
-            this.rank=new int[n];
-            Arrays.fill(this.rank,1);
-            this.f=new int[n];
-            for(int i=0;i<n;i++){
-                this.f[i]=i;
+            this.n = n;
+            this.rank = new int[n];
+            Arrays.fill(this.rank, 1);
+            this.f = new int[n];
+            for (int i = 0; i < n; i++) {
+                this.f[i] = i;
             }
         }
+
         public int find(int x) {
             return f[x] == x ? x : (f[x] = find(f[x]));
         }
+
         public boolean unionSet(int x, int y) {
             int fx = find(x), fy = find(y);
             if (fx == fy) {
@@ -135,36 +272,37 @@ public class S2 {
 
 
     }
-    public List<List<Integer>> threeSum(int[] nums) {
-        List res=new ArrayList();
 
-        int n= nums.length;
-        if(n==0)return res;
+    public List<List<Integer>> threeSum(int[] nums) {
+        List res = new ArrayList();
+
+        int n = nums.length;
+        if (n == 0) return res;
         Arrays.sort(nums);
-        for(int i=0;i<n-2;i++){
-            if(i>0&&nums[i]==nums[i-1]) i++;
-            int sum=-nums[i];
-            int j=i+1;
-            int k=n-1;
-            while (j<k){
-                if(nums[j]+nums[k]==sum){
-                    List<Integer> oneRes=new ArrayList<>();
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) i++;
+            int sum = -nums[i];
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                if (nums[j] + nums[k] == sum) {
+                    List<Integer> oneRes = new ArrayList<>();
                     oneRes.add(nums[i]);
                     oneRes.add(nums[j]);
                     oneRes.add(nums[k]);
                     res.add(oneRes);
                     j++;
-                    while (j<k&&nums[j]==nums[j-1]){
+                    while (j < k && nums[j] == nums[j - 1]) {
                         j++;
                     }
                     k--;
-                    while (j<k&&nums[k]==nums[k+1]){
+                    while (j < k && nums[k] == nums[k + 1]) {
                         k--;
                     }
-                }else if(nums[j]+nums[k]<sum){
+                } else if (nums[j] + nums[k] < sum) {
                     j++;
 
-                }else {
+                } else {
                     k--;
 
                 }
@@ -176,70 +314,77 @@ public class S2 {
         return res;
 
     }
+
     public int numSquares(int n) {
-        int[] dp= new int[n+1];
-        dp[0] =0;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
         Arrays.fill(dp, Integer.MAX_VALUE);
-        int max_square_index =(int) Math.sqrt(n)+1;
-        int square_num[] =new int[max_square_index];
-        for(int i=1;i<max_square_index;i++){
-            square_num[i]=i*i;
+        int max_square_index = (int) Math.sqrt(n) + 1;
+        int square_num[] = new int[max_square_index];
+        for (int i = 1; i < max_square_index; i++) {
+            square_num[i] = i * i;
         }
-        for(int i=1;i<=n;++i){
-            for(int s=1;s<max_square_index;++s){
-                if(i<square_num[s]){
+        for (int i = 1; i <= n; ++i) {
+            for (int s = 1; s < max_square_index; ++s) {
+                if (i < square_num[s]) {
                     break;
                 }
-                dp[i]=Math.min(dp[i],dp[i-square_num[s]]+1);
+                dp[i] = Math.min(dp[i], dp[i - square_num[s]] + 1);
             }
         }
         return dp[n];
     }
 
     public int maxCoins(int[] nums) {
-        int res=0;
-        int n =nums.length;
+        int res = 0;
+        int n = nums.length;
         return 0;
 
 
-
     }
+
     public int[] hitBricks(int[][] grid, int[][] hits) {
         return new int[0];
 
 
     }
+
     public int removeStones(int[][] stones) {
-        Union u =new Union();
-        for(int[] stone:stones){
-            u.union(stone[0]+10001,stone[1]);
+        Union u = new Union();
+        for (int[] stone : stones) {
+            u.union(stone[0] + 10001, stone[1]);
         }
-        return stones.length-u.getCount();
+        return stones.length - u.getCount();
 
     }
-    private class Union{
-        private Map<Integer,Integer> p;
+
+    private class Union {
+        private Map<Integer, Integer> p;
         private int count;
-        public Union(){
-            p=new HashMap<>();
-            this.count=0;
+
+        public Union() {
+            p = new HashMap<>();
+            this.count = 0;
 
         }
-        public int getCount(){
+
+        public int getCount() {
             return count;
 
         }
-        public int find(int x){
-            if(!p.containsKey(x)){
-                p.put(x,x);
+
+        public int find(int x) {
+            if (!p.containsKey(x)) {
+                p.put(x, x);
                 count++;
             }
-            if(x!=p.get(x)){
-                p.put(x,find(p.get(x)));
+            if (x != p.get(x)) {
+                p.put(x, find(p.get(x)));
             }
             return p.get(x);
 
         }
+
         public void union(int x, int y) {
             int rootX = find(x);
             int rootY = find(y);
@@ -253,44 +398,41 @@ public class S2 {
         }
 
 
-
-
-
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        TreeNode root = build(preorder,inorder,0,preorder.length,0,inorder.length);
+        TreeNode root = build(preorder, inorder, 0, preorder.length, 0, inorder.length);
 
         return root;
 
     }
 
-    private TreeNode build(int[] preorder, int[] inorder, int inStart, int inEnd,int preStart,int preEnd) {
+    private TreeNode build(int[] preorder, int[] inorder, int inStart, int inEnd, int preStart, int preEnd) {
 
-        if(preStart>=preEnd||inStart>=inEnd)
+        if (preStart >= preEnd || inStart >= inEnd)
             return null;
-        int mid =inStart;
-        for(;mid<inEnd;mid++){
-            if(inorder[mid]==preorder[preStart]){
+        int mid = inStart;
+        for (; mid < inEnd; mid++) {
+            if (inorder[mid] == preorder[preStart]) {
                 break;
             }
 
         }
-        int leftLen =mid-inStart;
-        TreeNode node =new TreeNode(preorder[preStart]);
-        node.left=build(preorder,inorder,inStart,mid,preStart+1,preStart+leftLen+1);
-        node.right=build(preorder,inorder,mid+1,inEnd,preStart+leftLen+1,preEnd);
+        int leftLen = mid - inStart;
+        TreeNode node = new TreeNode(preorder[preStart]);
+        node.left = build(preorder, inorder, inStart, mid, preStart + 1, preStart + leftLen + 1);
+        node.right = build(preorder, inorder, mid + 1, inEnd, preStart + leftLen + 1, preEnd);
         return node;
 
 
     }
 
     public List<Boolean> prefixesDivBy5(int[] A) {
-        List<Boolean> res =new ArrayList<>();
-        int num=0;
+        List<Boolean> res = new ArrayList<>();
+        int num = 0;
         for (int i = 0; i < A.length; i++) {
-            num=num*2+A[i];
-            res.add(num%5==0);
+            num = num * 2 + A[i];
+            res.add(num % 5 == 0);
 
 
         }
@@ -299,23 +441,22 @@ public class S2 {
     }
 
     int resDiameter;
+
     public int diameterOfBinaryTree(TreeNode root) {
-        resDiameter=1;
+        resDiameter = 1;
         dfs(root);
         // nodeNum -1 is res ;
-        return resDiameter-1;
-
-
+        return resDiameter - 1;
 
 
     }
 
-    private int  dfs(TreeNode root) {
-        if(root==null) return 0;
-        int L =dfs(root.left);
-        int R =dfs(root.right);
-        resDiameter=Math.max(resDiameter,L+R+1);
-        return Math.max(L,R)+1;
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int L = dfs(root.left);
+        int R = dfs(root.right);
+        resDiameter = Math.max(resDiameter, L + R + 1);
+        return Math.max(L, R) + 1;
 
     }
 
@@ -323,22 +464,23 @@ public class S2 {
         return 0;
 
     }
+
     public int[] findRedundantConnection(int[][] edges) {
-         int n = edges.length;
-         int[] p=new int[n+1];
-        for (int i = 1; i <n+1 ; i++) {
-            p[i]=i;
+        int n = edges.length;
+        int[] p = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            p[i] = i;
 
         }
-        for(int i=0;i<n;i++){
-            int[] edge =edges[i];
-            int node1=edge[0];
-            int node2=edge[1];
-            int f1=find(p,node1);
-            int f2=find(p,node2);
-            if(f1!=f2){
-                p[f1]=f2;
-            }else{
+        for (int i = 0; i < n; i++) {
+            int[] edge = edges[i];
+            int node1 = edge[0];
+            int node2 = edge[1];
+            int f1 = find(p, node1);
+            int f2 = find(p, node2);
+            if (f1 != f2) {
+                p[f1] = f2;
+            } else {
                 return edge;
             }
         }
@@ -348,33 +490,33 @@ public class S2 {
     }
 
     private int find(int[] p, int node) {
-        while (p[node]!=node){
-            p[node]=find(p,p[node]);
-            node=p[node];
+        while (p[node] != node) {
+            p[node] = find(p, p[node]);
+            node = p[node];
 
         }
         return node;
     }
 
     public int[] dailyTemperatures(int[] T) {
-        int n=T.length;
-        Deque<Integer> deque =new LinkedList<>();
-        int[] res=new int[n];
-        res[n-1] =0;
-        deque.addLast(n-1);
-        for(int i=n-2;i>=0;i--){
-            if(T[deque.peekFirst()]>T[i]){
-                res[i]=1;
+        int n = T.length;
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[n];
+        res[n - 1] = 0;
+        deque.addLast(n - 1);
+        for (int i = n - 2; i >= 0; i--) {
+            if (T[deque.peekFirst()] > T[i]) {
+                res[i] = 1;
                 deque.addFirst(i);
-            }else {
-                while (!deque.isEmpty()&&T[deque.peekFirst()]<=T[i]){
+            } else {
+                while (!deque.isEmpty() && T[deque.peekFirst()] <= T[i]) {
                     deque.pollFirst();
                 }
-                if(deque.isEmpty()){
-                    res[i]=0;
+                if (deque.isEmpty()) {
+                    res[i] = 0;
                     deque.addFirst(i);
-                }else {
-                    res[i]=deque.peekFirst()-i;
+                } else {
+                    res[i] = deque.peekFirst() - i;
                     deque.addFirst(i);
                 }
             }
@@ -384,12 +526,14 @@ public class S2 {
 
 
     }
-    int sum=0;
+
+    int sum = 0;
+
     public TreeNode bstToGst(TreeNode root) {
-        if(root !=null){
+        if (root != null) {
             bstToGst(root.right);
-            sum+=root.val;
-            root.val=sum;
+            sum += root.val;
+            root.val = sum;
             bstToGst(root.left);
 
         }
@@ -397,10 +541,11 @@ public class S2 {
 
 
     }
+
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList();
         Queue<TreeNode> queue = new LinkedList();
-        if(root==null) return res;
+        if (root == null) return res;
         queue.add(root);
         while (!queue.isEmpty()) {
             int size = queue.size();
@@ -410,11 +555,11 @@ public class S2 {
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
                 oneLine.add(node.val);
-                if(node.left!=null){
+                if (node.left != null) {
                     queue.add(node.left);
 
                 }
-                if(node.right!=null){
+                if (node.right != null) {
                     queue.add(node.right);
 
                 }
@@ -429,7 +574,7 @@ public class S2 {
     public List<List<Integer>> levelOrder(Node root) {
         List<List<Integer>> res = new ArrayList();
         Queue<Node> queue = new LinkedList();
-        if(root==null) return res;
+        if (root == null) return res;
         queue.add(root);
         while (!queue.isEmpty()) {
             int size = queue.size();
@@ -437,7 +582,7 @@ public class S2 {
             for (int i = 0; i < size; i++) {
                 Node node = queue.poll();
                 oneLine.add(node.val);
-                for (int j = 0; j <node.children.size() ; j++) {
+                for (int j = 0; j < node.children.size(); j++) {
                     queue.add(node.children.get(i));
 
                 }
